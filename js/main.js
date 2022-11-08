cbca3c93d3d7eabb3e63b525bd9c90eb1fd0c79c
AlexandruChiriac98 <achiriac98@gmail.com>
2022-11-08 15:36:44 +0100

    .

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const Nome = prompt("Inserisci Nome")
+const Cognome = prompt("Inserisci Cognome")
+const ColorePreferito = prompt("Inserisci Colore Preferito")
+
+let Password = Nome + Cognome + ColorePreferito + 21;
+console.log(Password)
