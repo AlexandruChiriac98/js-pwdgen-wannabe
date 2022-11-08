bcd968be28733c723d77602e37d3c48ef07421b8
AlexandruChiriac98 <achiriac98@gmail.com>
2022-11-08 16:18:41 +0100

    fatto

--- js/main.js
+++ js/main.js
@@ -4,3 +4,6 @@ const ColorePreferito = prompt("Inserisci Colore Preferito")
 
 let Password = Nome + Cognome + ColorePreferito + 21;
 console.log(Password)
+
+const h2Pass = document.getElementById("Pass")
+h2Pass.innerHTML = Password
